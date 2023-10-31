9085cacde680aee8c932669371cbb8760d0d7e8a
bigmistqke <vandijckv@gmail.com>
2023-10-31 01:23:29 +0100

    cleanup types

--- src/types.ts
+++ src/types.ts
@@ -1,21 +1,6 @@
 import { Vector } from '@lib/spagett/types'
 
-export type Handle = { handleId: string; nodeId: string }
-export type Edge = { start: Handle; end: Handle }
-export type Parameter<T = any> = { type: string; value: T }
-
-export type Nodes = Record<
-  string,
-  {
-    position: Vector
-    func: (parameters: Record<string, any>) => any
-    parameters: Record<string, Parameter>
-    returnType: DataType
-  }
->
-
 export type Func = (...args: any[]) => any
-
 export type DataType = 'string' | 'number'
 
 export interface CodeAtom {
@@ -32,4 +17,15 @@ export interface NetworkAtom extends CodeAtom {
 
 export type Atom = CodeAtom | NetworkAtom
 
+export type Node = Atom & {
+  position: Vector
+}
+export type Nodes = Record<string, Node>
+
 export type Package = Record<string, Atom>
+
+/* NETWORK */
+
+export type Handle = { handleId: string; nodeId: string }
+export type Edge = { start: Handle; end: Handle }
+export type Parameter<T = any> = { type: string; value: T }
